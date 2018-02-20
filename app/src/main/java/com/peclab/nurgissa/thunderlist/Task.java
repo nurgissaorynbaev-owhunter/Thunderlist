42dae133ad6a3fc63ad2f8b279749e4e0645d6c1
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-02-20 21:22:58 +0600

    Add Task.java

---
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/Task.java
@@ -0,0 +1,23 @@
+package com.peclab.nurgissa.thunderlist;
+
+
+public class Task {
+    private String content;
+
+    public Task(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+}
