917ca5fb2a65b1282452a73450bd1c1e0e532246
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-02-21 16:24:27 +0600

    change content filed to title Task.java

--- app/src/main/java/com/peclab/nurgissa/thunderlist/Task.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/Task.java
@@ -2,22 +2,22 @@ package com.peclab.nurgissa.thunderlist;
 
 
 public class Task {
-    private String content;
+    private String title;
 
-    public Task(String content) {
-        this.content = content;
+    public Task(String title) {
+        this.title = title;
     }
 
-    public String getContent() {
-        return content;
+    public String getTitle() {
+        return title;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     @Override
     public String toString() {
-        return content;
+        return title;
     }
 }
