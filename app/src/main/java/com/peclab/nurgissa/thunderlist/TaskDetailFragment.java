313569623d97aeb4ee445e90674a04719d5dd1b1
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-03 10:59:04 +0600

    Add method listener for Note clicked TaskDetailFragment.java

--- app/src/main/java/com/peclab/nurgissa/thunderlist/TaskDetailFragment.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/TaskDetailFragment.java
@@ -1,19 +1,24 @@
 package com.peclab.nurgissa.thunderlist;
 
 
-import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
-public class TaskDetailFragment extends Fragment implements TaskDetailRecyclerViewAdapter.Listener {
+public class TaskDetailFragment extends Fragment implements TaskDetailContract.View, TaskDetailRecyclerViewAdapter.Listener {
     public static final String EXTRA_VALUE = "value";
     private TaskDetailPresenter presenter;
+    private TaskDetailRecyclerViewAdapter adapter;
 
     public TaskDetailFragment() {
         // Required empty public constructor
@@ -27,11 +32,11 @@ public class TaskDetailFragment extends Fragment implements TaskDetailRecyclerVi
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_task_detail, container, false);
         LinearLayoutManager layoutManager = new LinearLayoutManager(inflater.getContext());
 
-        presenter = new TaskDetailPresenter();
+        presenter = new TaskDetailPresenter(this);
 
-        feelRecyclerViewByDefaultData(value);
+        initializeRecyclerView(value);
 
-        TaskDetailRecyclerViewAdapter adapter = new TaskDetailRecyclerViewAdapter(this, presenter);
+        adapter = new TaskDetailRecyclerViewAdapter(this, presenter);
 
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
 
@@ -43,11 +48,11 @@ public class TaskDetailFragment extends Fragment implements TaskDetailRecyclerVi
         return recyclerView;
     }
 
-    private void feelRecyclerViewByDefaultData(String value) {
-        presenter.addDetailTask(R.drawable.note_96, value);
-        presenter.addDetailTask(R.drawable.schedule_96, getString(R.string.reminder_hint));
-        presenter.addDetailTask(R.drawable.note_96, getString(R.string.note_hint));
-        presenter.addDetailTask(R.drawable.plus_96, getString(R.string.subTask_hint));
+    private void initializeRecyclerView(String value) {
+        presenter.addDetailTaskItem(R.drawable.task_value, value, DetailTaskItem.VALUE);
+        presenter.addDetailTaskItem(R.drawable.task_shedule, getString(R.string.reminder_hint), DetailTaskItem.SCHEDULE);
+        presenter.addDetailTaskItem(R.drawable.task_note, getString(R.string.note_hint), DetailTaskItem.NOTE);
+        presenter.addDetailTaskItem(R.drawable.add_subtask, getString(R.string.sub_task_hint), DetailTaskItem.ADD_SUBTASK);
     }
 
     @Override
@@ -57,5 +62,42 @@ public class TaskDetailFragment extends Fragment implements TaskDetailRecyclerVi
                 .replace(R.id.fragment_container, fragment, null)
                 .addToBackStack(null)
                 .commit();
+    }
+
+    @Override
+    public void onAddSubtaskItemClick(final View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final EditText input = new EditText(getContext());
+        final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(getContext().INPUT_METHOD_SERVICE);
+
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setTitle(getResources().getString(R.string.sub_task_hint));
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String value = input.getText().toString();
+                presenter.addSubtask(value);
+
+                imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
+            }
+        });
+
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+    }
+
+    @Override
+    public void notifySubtaskAddedToDetailTask() {
+        adapter.notifyDataSetChanged();
     }
 }
