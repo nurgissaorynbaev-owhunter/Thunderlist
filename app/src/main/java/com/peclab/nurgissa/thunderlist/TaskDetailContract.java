9404d9389f9c8532fc25297bffbcf2c11901bd28
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-03 10:58:19 +0600

    Add View interface TaskDetailContract.java

--- app/src/main/java/com/peclab/nurgissa/thunderlist/TaskDetailContract.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/TaskDetailContract.java
@@ -3,11 +3,19 @@ package com.peclab.nurgissa.thunderlist;
 
 public interface TaskDetailContract {
 
+    interface View {
+        void notifySubtaskAddedToDetailTask();
+    }
+
     interface BasicAdapterView {
         void feelView(int image, String value);
     }
 
     interface SubtaskAdapterView {
         void feelView(String value);
+    }
+
+    interface BasicAdapterValueView {
+        void feelView(int image, String value);
     }
 }
