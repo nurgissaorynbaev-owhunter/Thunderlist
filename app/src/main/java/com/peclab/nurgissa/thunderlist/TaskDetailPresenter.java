bd6d36b9c2e23fe9dba3d6d573f4e73741c28d5d
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-12 21:01:28 +0600

    Separatly populate TaskDetail object in particular methods TaskDetailPresenter.java

--- app/src/main/java/com/peclab/nurgissa/thunderlist/TaskDetailPresenter.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/TaskDetailPresenter.java
@@ -4,49 +4,136 @@ package com.peclab.nurgissa.thunderlist;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TaskDetailPresenter {
-    private List<DetailTaskItem> detailTaskItems;
+public class TaskDetailPresenter implements TaskDetailContract.Interactor.OnFinishedListener {
+    private List<TaskDetail> taskDetails;
     private TaskDetailContract.View view;
+    private TaskDetailContract.Interactor interactor;
 
-    public TaskDetailPresenter(TaskDetailContract.View view) {
+    public TaskDetailPresenter(TaskDetailContract.View view, TaskDetailContract.Interactor interactor) {
         this.view = view;
-        detailTaskItems = new ArrayList<>();
+        this.interactor = interactor;
+        taskDetails = new ArrayList<>();
     }
 
-    public void bindTaskTitleViewHolderToValue(TaskDetailRecyclerViewAdapter.TaskTitleViewHolder adapterView, int position) {
-        DetailTaskItem dt = detailTaskItems.get(position);
-        adapterView.feelView(dt.getImage(), dt.getText());
+    public void bindTaskTitleViewToValue(TaskDetailRecyclerViewAdapter.TaskTitleViewHolder adapterView, int position) {
+        TaskDetail td = taskDetails.get(position);
+
+        adapterView.setImage(td.getImage());
+
+        if (td.getText().equals("Task title..")) {
+            adapterView.setTextHint(td.getText());
+        } else {
+            adapterView.setText(td.getText());
+        }
     }
 
-    public void bindBasicViewHolderToValue(TaskDetailContract.BasicAdapterView adapterView, int position) {
-        DetailTaskItem dt = detailTaskItems.get(position);
-        adapterView.feelView(dt.getImage(), dt.getText());
+    public void bindBasicViewToValue(TaskDetailContract.BasicAdapterView adapterView, int position) {
+        TaskDetail td = taskDetails.get(position);
+        adapterView.setText(td.getText());
+        adapterView.setImage(td.getImage());
     }
 
-    public void bindSubtaskViewHolderToValue(TaskDetailContract.SubtaskAdapterView adapterView, int position) {
-        DetailTaskItem dt = detailTaskItems.get(position);
-        adapterView.feelView(dt.getText());
+    public void bindSubtaskViewToValue(TaskDetailContract.SubtaskAdapterView adapterView, int position) {
+        TaskDetail td = taskDetails.get(position);
+        adapterView.setText(td.getText());
     }
 
     public void addDetailTaskItem(int image, String value, int viewType) {
-        detailTaskItems.add(new DetailTaskItem(image, value, viewType));
+//        Task task = new Task();
+//        task.setText(value);
+//        task.setImage(image);
+//        task.setItemViewType(new DetailItemViewType(viewType));
+//
+//        Task result = interactor.create(task);
+//
+//        taskDetails.add(result);
     }
 
     public int getDetailTaskItemCount() {
-        return detailTaskItems.size();
+        return taskDetails.size();
     }
 
     public int getViewType(int position) {
-        return detailTaskItems.get(position).getViewType();
+        return taskDetails.get(position).getViewType();
     }
 
     public void addSubtask(String value) {
-        DetailTaskItem detailItem = new DetailTaskItem();
-        detailItem.setText(value);
-        detailItem.setViewType(DetailTaskItem.SUBTASK);
+//        DetailTaskItem detailItem = new DetailTaskItem();
+//        detailItem.setText(value);
+//        detailItem.setType(DetailTaskItem.SUBTASK);
+//
+//        taskDetails.add(DetailTaskItem.SUBTASK, detailItem);
+//
+//        view.notifySubtaskAddedToDetailTask();
+    }
 
-        detailTaskItems.add(DetailTaskItem.SUBTASK, detailItem);
+    @Override
+    public void onCreateFinished() {
+        System.out.println("Show toast -> task created!");
+    }
 
-        view.notifySubtaskAddedToDetailTask();
+    public void initializeTaskDetail(String title) {
+        Task task = new Task();
+        if (title != null) {
+            task = interactor.getByTitle(title);
+        }
+
+        populateEditValueItem(task);
+        populateScheduleItem(task);
+        populateNoteItem(task);
+        populateAddSubtaskItem(task);
+    }
+
+    private void populateAddSubtaskItem(Task task) {
+        TaskDetail td = new TaskDetail();
+        td.setText("Add a subtask");
+        td.setViewType(TaskDetail.ADD_SUBTASK);
+        td.setImage(TaskDetail.ADD_SUBTASK_IMAGE);
+
+        taskDetails.add(td);
+    }
+
+    private void populateNoteItem(Task task) {
+        TaskDetail td = new TaskDetail();
+
+        if (task.getNote() != null) {
+            td.setText(task.getNote());
+        } else {
+            td.setText("Add a note..");
+        }
+
+        td.setViewType(TaskDetail.NOTE);
+        td.setImage(TaskDetail.NOTE_IMAGE);
+
+        taskDetails.add(td);
+    }
+
+    private void populateScheduleItem(Task task) {
+        TaskDetail td = new TaskDetail();
+
+        if (task.getDateTime() != null) {
+            td.setText(String.valueOf(task.getDateTime()));
+        } else {
+            td.setText("Set Reminder");
+        }
+
+        td.setViewType(TaskDetail.SCHEDULE);
+        td.setImage(TaskDetail.SCHEDULE_IMAGE);
+
+        taskDetails.add(td);
+    }
+
+    private void populateEditValueItem(Task task) {
+        TaskDetail td = new TaskDetail();
+
+        if (task.getTitle() != null) {
+            td.setText(task.getTitle());
+        } else {
+            td.setText("Task title..");
+        }
+        td.setViewType(TaskDetail.EDIT_VALUE);
+        td.setImage(TaskDetail.EDIT_VALUE_IMAGE);
+
+        taskDetails.add(td);
     }
 }
