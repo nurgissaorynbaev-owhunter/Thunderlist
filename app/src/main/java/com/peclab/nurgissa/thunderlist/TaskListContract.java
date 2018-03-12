407e68f67e482656d5ede03c460683379090895d
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-12 21:03:32 +0600

    Add Interactor interface TaskListContract.java

--- app/src/main/java/com/peclab/nurgissa/thunderlist/TaskListContract.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/TaskListContract.java
@@ -1,6 +1,8 @@
 package com.peclab.nurgissa.thunderlist;
 
 
+import java.util.List;
+
 public interface TaskListContract {
 
     interface View {
@@ -11,10 +13,24 @@ public interface TaskListContract {
 
     interface Presenter {
         void addQuickTask(String value);
+        int getTasksCount();
+        void bindAdapterViewToValue(TaskListContract.AdapterView adapterView, int position);
+        void checkStatusChanged(boolean isChecked, int position);
+        void taskClicked(int position);
     }
 
     interface AdapterView {
         void setTitle(String value);
         void setChecked(boolean value);
+    }
+
+    interface Interactor {
+        interface OnFinishedListener {
+            void onCreateFinished();
+        }
+
+        Task create(Task task, OnFinishedListener onFinishedListener);
+        Task get(int id);
+        List<Task> getAll();
     }
 }
