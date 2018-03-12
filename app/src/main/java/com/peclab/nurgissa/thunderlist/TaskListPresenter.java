b8467379fcaee45b4997fe392027748c054088df
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-12 21:05:40 +0600

    Implement taskClicked, getTaskCount, checkStatusChanged methods

--- app/src/main/java/com/peclab/nurgissa/thunderlist/TaskListPresenter.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/TaskListPresenter.java
@@ -4,17 +4,27 @@ package com.peclab.nurgissa.thunderlist;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TaskListPresenter implements TaskListContract.Presenter {
+public class TaskListPresenter implements TaskListContract.Presenter, TaskListContract.Interactor.OnFinishedListener {
     private TaskListContract.View view;
+    private TaskListContract.Interactor interactor;
     private List<Task> tasks;
     private TaskListContract.AdapterView adapterView;
 
 
-    public TaskListPresenter(TaskListContract.View view) {
+    public TaskListPresenter(TaskListContract.View view, TaskListContract.Interactor interactor) {
         this.tasks = new ArrayList<>();
         this.view = view;
+        this.interactor = interactor;
+
+        getAll();
     }
 
+    private void getAll() {
+        List<Task> list = interactor.getAll();
+        tasks.addAll(list);
+    }
+
+    @Override
     public void bindAdapterViewToValue(TaskListContract.AdapterView adapterView, int position) {
         this.adapterView = adapterView;
 
@@ -27,10 +37,13 @@ public class TaskListPresenter implements TaskListContract.Presenter {
         Task task = new Task();
         task.setTitle(value);
 
-        tasks.add(0, task);
+        Task result = interactor.create(task, this);
+        tasks.add(0, result);
+
         view.notifyDataAddedToTaskList(0);
     }
 
+    @Override
     public void checkStatusChanged(boolean value, int position) {
         if (value) {
             tasks.remove(position);
@@ -40,11 +53,17 @@ public class TaskListPresenter implements TaskListContract.Presenter {
         }
     }
 
+    @Override
     public int getTasksCount() {
         return tasks.size();
     }
 
+    @Override
     public void taskClicked(int position) {
         view.deliverTaskTitle(tasks.get(position).getTitle());
+    }
+
+    @Override
+    public void onCreateFinished() {
     }
 }
