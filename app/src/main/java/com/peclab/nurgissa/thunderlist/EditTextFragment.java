bebe758bb4e964afa5d674837fe203fa8e921534
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-15 11:42:56 +0600

    Add toolbar EditTextFragment.java, add inner listener EditTextFragment.java

--- app/src/main/java/com/peclab/nurgissa/thunderlist/EditTextFragment.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/EditTextFragment.java
@@ -2,10 +2,16 @@ package com.peclab.nurgissa.thunderlist;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
@@ -15,30 +21,70 @@ import android.widget.TextView;
 
 public class EditTextFragment extends Fragment {
     private EditText editText;
-    private Context context;
     private InputMethodManager imm;
+    private Listener listener;
+
+    interface Listener {
+        void getText(String value);
+    }
 
     public EditTextFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener = (Listener) getActivity();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        context = inflater.getContext();
         View view = inflater.inflate(R.layout.fragment_edit_text, container, false);
+
+        setToolbar(view);
 
         editText = view.findViewById(R.id.detail_edit_text);
         editText.requestFocus();
 
         imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
 
         handleEditText();
 
         return view;
+    }
+
+    private void setToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.toolbar_edit_text);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_edit_text, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                getActivity().onBackPressed();
+                return true;
+
+            case R.id.menu_item_edit_text_accept_action:
+                listener.getText(editText.getText().toString());
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void handleEditText() {
