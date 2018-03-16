26be44e93d243c12ad093745774351db1b61b1aa
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-16 17:37:07 +0600

    Add DetailContract.java

---
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/DetailContract.java
@@ -0,0 +1,29 @@
+package com.peclab.nurgissa.thunderlist;
+
+
+public interface DetailContract {
+
+    interface View {
+        void moveToMainList();
+        void initDetailTitle(String title);
+        void initDetailReminder(String reminder);
+        void initDetailNote(String note);
+    }
+
+    interface Presenter {
+        void initDetailFromDatabase(String title);
+        void addTextTitle(String title);
+        void addTextNote(String note);
+        void saveDetail();
+        void addReminderDate(String date);
+        void addReminderTime(String time);
+    }
+
+    interface Interactor {
+        interface OnFinishedListener {
+            void onSaveFinished();
+        }
+        void save(OnFinishedListener onFinishedListener, Task task);
+        Task getByTitle(String title);
+    }
+}
