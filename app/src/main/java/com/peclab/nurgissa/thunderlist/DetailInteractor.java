04b66149be4e318b78403ad247515221ce469c6a
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-16 17:36:34 +0600

    Add DetailFragment.java,

---
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/DetailInteractor.java
@@ -0,0 +1,59 @@
+package com.peclab.nurgissa.thunderlist;
+
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DetailInteractor implements DetailContract.Interactor {
+    private static final String TABLE_NAME = "Task";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_REMINDER_DATE = "reminder_date";
+    private static final String COLUMN_REMINDER_TIME = "reminder_time";
+    private static final String COLUMN_NOTE = "note";
+
+
+    @Override
+    public void save(DetailContract.Interactor.OnFinishedListener onFinishedListener, Task task) {
+        SQLiteDatabase db = DBConnection.getConnection().getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COLUMN_TITLE, task.getTitle());
+        contentValues.put(COLUMN_REMINDER_DATE, task.getReminderDate());
+        contentValues.put(COLUMN_REMINDER_TIME, task.getReminderTime());
+        contentValues.put(COLUMN_NOTE, task.getNote());
+
+        db.insert(TABLE_NAME, null, contentValues);
+
+        onFinishedListener.onSaveFinished();
+
+        db.close();
+    }
+
+    @Override
+    public Task getByTitle(String title) {
+        SQLiteDatabase db = DBConnection.getConnection().getReadableDatabase();
+        Task task = new Task();
+
+        Cursor cursor = db.query(TABLE_NAME, new String[]{COLUMN_ID, COLUMN_TITLE, COLUMN_REMINDER_DATE, COLUMN_REMINDER_TIME, COLUMN_NOTE}, COLUMN_TITLE + "=?", new String[]{title}, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            int id = Integer.parseInt(cursor.getString(0));
+            String t = cursor.getString(1);
+            String date = cursor.getString(2);
+            String time = cursor.getString(3);
+            String note = cursor.getString(4);
+
+            task.setId(id);
+            task.setTitle(t);
+            task.setReminderDate(date);
+            task.setReminderTime(time);
+            task.setNote(note);
+        }
+        cursor.close();
+        db.close();
+
+        return task;
+    }
+}
---
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/DetailPresenter.java
@@ -0,0 +1,59 @@
+package com.peclab.nurgissa.thunderlist;
+
+
+public class DetailPresenter implements DetailContract.Presenter, DetailContract.Interactor.OnFinishedListener {
+    private DetailContract.View view;
+    private DetailContract.Interactor interactor;
+    private Task task;
+
+    public DetailPresenter(DetailContract.View view, DetailContract.Interactor interactor) {
+        this.view = view;
+        this.interactor = interactor;
+        this.task = new Task();
+    }
+
+    @Override
+    public void initDetailFromDatabase(String title) {
+        task = interactor.getByTitle(title);
+
+        view.initDetailTitle(task.getTitle());
+
+        if (task.getReminderDate() != null)
+            view.initDetailReminder(task.getReminderDate() + " at " + task.getReminderTime());
+
+        if (task.getNote() != null)
+            view.initDetailNote(task.getNote());
+    }
+
+    @Override
+    public void addTextTitle(String title) {
+        task.setTitle(title);
+    }
+
+    @Override
+    public void addTextNote(String note) {
+        task.setNote(note);
+    }
+
+    @Override
+    public void addReminderDate(String date) {
+        task.setReminderDate(date);
+    }
+
+    @Override
+    public void addReminderTime(String time) {
+        task.setReminderTime(time);
+
+        view.initDetailReminder(task.getReminderDate() + " at " + task.getReminderTime());
+    }
+
+    @Override
+    public void saveDetail() {
+        interactor.save(this, task);
+    }
+
+    @Override
+    public void onSaveFinished() {
+        view.moveToMainList();
+    }
+}
