ecfb214bbcbd387785a2864ca5731eaffafe598c
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-17 09:59:48 +0600

    Add interface Precenter, Interactor NavDrawerContract.java

--- app/src/main/java/com/peclab/nurgissa/thunderlist/NavDrawerContract.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/NavDrawerContract.java
@@ -1,14 +1,28 @@
 package com.peclab.nurgissa.thunderlist;
 
 
+import java.util.List;
+
 public interface NavDrawerContract {
 
     interface View {
         void createNewItemCategory(int position);
         void notifyItemCategoryAdded(int position);
+        void notifyCategoryDataChanged();
     }
 
     interface AdapterView {
         void setItemNav(String title, int imageView, int imageColor, int itemCount);
+    }
+
+    interface Presenter {
+        void initDefaultCategories(List<Category> categories);
+        boolean doesCategoriesExist();
+    }
+
+    interface Interactor {
+        void initDefaultCategory(List<Category> categories);
+        void add(Category category);
+        List<Category> getAll();
     }
 }
