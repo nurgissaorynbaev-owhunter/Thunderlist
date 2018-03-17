7d67e8248433598b1ffdc98147de4fffbc498bb4
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-17 09:57:03 +0600

    Change field names, add id field Category.java

---
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/Category.java
@@ -0,0 +1,58 @@
+package com.peclab.nurgissa.thunderlist;
+
+
+class Category {
+    private int id;
+    private String name;
+    private int image;
+    private int taskCount;
+    private int imageColor;
+
+    public Category () {}
+
+    public Category(String name, int image, int imageColor) {
+        this.name = name;
+        this.image = image;
+        this.imageColor = imageColor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getTaskCount() {
+        return taskCount;
+    }
+
+    public void setTaskCount(int taskCount) {
+        this.taskCount = taskCount;
+    }
+
+    public int getImageColor() {
+        return imageColor;
+    }
+
+    public void setImageColor(int imageColor) {
+        this.imageColor = imageColor;
+    }
+}
