b6b0646bc9b8a0343928adf0e469a5424258dc01
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-17 09:53:29 +0600

    Add doesCategoryExist method, initDefaultCategories, add Interactor instance NavDrawerPresenter.java

--- app/src/main/java/com/peclab/nurgissa/thunderlist/NavDrawerPresenter.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/NavDrawerPresenter.java
@@ -2,29 +2,52 @@ package com.peclab.nurgissa.thunderlist;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class NavDrawerPresenter {
+public class NavDrawerPresenter implements NavDrawerContract.Presenter {
     private NavDrawerContract.View view;
-    private List<TaskCategory> categories;
+    private NavDrawerContract.Interactor interactor;
+    private List<Category> categories;
 
-    public NavDrawerPresenter(NavDrawerContract.View view) {
+    public NavDrawerPresenter(NavDrawerContract.View view, NavDrawerContract.Interactor interactor) {
         this.view = view;
+        this.interactor = interactor;
         categories = new ArrayList<>();
     }
 
-    public void bindAdapterViewToData(NavDrawerContract.AdapterView adapterView, int position) {
-        TaskCategory taskCategory = categories.get(position);
-        adapterView.setItemNav(taskCategory.getTitle(), taskCategory.getImage(), taskCategory.getImageColor(), taskCategory.getItemCount());
+    @Override
+    public boolean doesCategoriesExist() {
+        List<Category> categories = interactor.getAll();
+        if (!categories.isEmpty()) {
+            Collections.swap(categories, 4, categories.size() - 1);
+            this.categories = categories;
+            view.notifyCategoryDataChanged();
+
+            return true;
+        }
+        return false;
     }
 
-    public void addNavDrawerCategory(TaskCategory taskCategory, int position) {
-        categories.add(position, taskCategory);
+    public void bindAdapterViewToData(NavDrawerContract.AdapterView adapterView, int position) {
+        Category category = categories.get(position);
+        adapterView.setItemNav(category.getName(), category.getImage(), category.getImageColor(), category.getTaskCount());
+    }
+
+    public void addNavDrawerCategory(Category category, int position) {
+        categories.add(position, category);
+
+        interactor.add(category);
+
         view.notifyItemCategoryAdded(position);
     }
 
-    public void addNavDrawerCategory(TaskCategory taskCategory) {
-        categories.add(taskCategory);
+    @Override
+    public void initDefaultCategories(List<Category> categories) {
+        this.categories = categories;
+        interactor.initDefaultCategory(categories);
+
+        view.notifyCategoryDataChanged();
     }
 
     public int getCategoryItemCount() {
