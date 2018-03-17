851b5e050479545feece0a651e64d785a923487b
Nurgissa Orynbaev <nurgissao@gmail.com>
2018-03-17 09:58:17 +0600

    Add category table, do singleton DatabaseHelper.java

--- app/src/main/java/com/peclab/nurgissa/thunderlist/DatabaseHelper.java
+++ app/src/main/java/com/peclab/nurgissa/thunderlist/DatabaseHelper.java
@@ -6,8 +6,18 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "Thunderlist";
+    private static final int DATABASE_VERSION = 1;
+    private static DatabaseHelper instance;
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public static synchronized DatabaseHelper getInstance(Context context) {
+        if (instance == null) {
+           instance = new DatabaseHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        return instance;
+    }
+
+    private DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
@@ -19,11 +29,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 "reminder_date text, " +
                 "reminder_time text, " +
                 "note text);");
+
+        db.execSQL("Create table Category (" +
+                "_id integer primary key autoincrement, " +
+                "name text, " +
+                "image int, " +
+                "image_color int, " +
+                "task_count integer);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("Drop table if exists " + "Task");
+        db.execSQL("Drop table if exists " + "Category");
         onCreate(db);
     }
 }
